acf3a6ddb2ddae7d5326b9d020106aa1d6f7cb10
SemiRaden <radenbacompanado@su.edu.ph>
2024-07-18 23:14:15 +0800

    feat: delete action in store

--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,23 +1,23 @@
-// stores/ProductStore.ts
 import { defineStore } from 'pinia'
+import axios from 'axios'
 import { type Product } from '@/types/Product'
 
-export const useProductStore = defineStore('productStore', {
+export const useProductStore = defineStore('product', {
   state: () => ({
     products: [] as Product[]
   }),
   actions: {
     async fetchProducts() {
       try {
-        const response = await fetch('https://dummyjson.com/products')
-        if (!response.ok) {
-          throw new Error(`Failed to fetch products: ${response.statusText}`)
-        }
-        const data: Product[] = await response.json()
-        this.products = data
+        const response = await axios.get('https://dummyjson.com/products')
+        this.products = response.data.products
       } catch (error) {
         console.error('Error fetching products:', error)
       }
+    },
+    deleteProduct(productId: number) {
+      this.products = this.products.filter((product) => product.id !== productId)
+      console.log(`Deleted product with ID: ${productId}`)
     }
   }
 })
