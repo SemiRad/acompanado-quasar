6b78ff6e1b022dcc2f4bbf87eba3221f9907ebd2
SemiRaden <radenbacompanado@su.edu.ph>
2024-07-20 00:08:07 +0800

    ui: applying quasar as a global use

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { Quasar } from 'quasar'
 import App from './App.vue'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
+app.use(Quasar, {
+  plugins: {}
+})
+
 app.mount('#app')
